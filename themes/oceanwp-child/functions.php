f8d348684d3bcb6f0ecaa2b1ea2395dc881ee821
Plamena Hristova <eaapdh@students.eaaa.dk>
2019-09-13 15:15:46 +0200

    I have uploaded my 1st version of Wordpress themes folder

---
+++ themes/oceanwp-child/functions.php
@@ -0,0 +1,16 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+ 
+ // injecting the Mother theme's stylesheet
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    
+ // injecting child theme's stylesheet
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
